8722b04734c4b60b553a778aadcbdc2aeffb6e92
SanniMuhammed <young.muhammedd@gmail.com>
2026-09-06 21:03:16 +0100

    chore: prepare website for Next.js 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // TODO: once real product photography is hosted somewhere,
-    // add that host here, e.g.:
-    // remotePatterns: [{ protocol: 'https', hostname: 'cdn.example.com' }]
-  },
+  images: {},
 };
 
 export default nextConfig;
